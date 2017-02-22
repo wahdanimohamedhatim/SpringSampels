1ba92456502fe95162c331589d1a994d919ce4cd
Hatim WAHDANI <wahdani.mohamed.hatim@gmail.com>
2017-02-22 07:22:18 +0000

    bean with contructor-arg and propertie at the same time

--- SpringIoCContainer/src/main/java/wahdanimohamedhatim/SpringIoCContainer/plugin/v1/MoteurImpl.java
+++ SpringIoCContainer/src/main/java/wahdanimohamedhatim/SpringIoCContainer/plugin/v1/MoteurImpl.java
@@ -5,7 +5,7 @@ import wahdanimohamedhatim.SpringIoCContainer.IMoteur;
 public class MoteurImpl implements IMoteur {
 	
 	private int nbrLitre;
-	
+	private int nbrChevaux;
 public void setNbrLitre(int nbrLitre) {
 		this.nbrLitre = nbrLitre;
 	}
@@ -16,9 +16,16 @@ public void setNbrLitre(int nbrLitre) {
 //		
 //	}
 //	
+
+public MoteurImpl(int nbrChevaux){
+this.nbrChevaux=nbrChevaux;
+System.out.println("Constructor MoteurImpl ch: " + this.nbrChevaux);
+
+}
+
 	public String toString(){
 		if(this!=null)
-		return " Litres : "+this.nbrLitre;
+		return " Litres : "+this.nbrLitre+" Chevaux : "+this.nbrChevaux;
 		return "NON";
 	}
 
